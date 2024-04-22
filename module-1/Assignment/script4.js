58a860b70adbed66e14fab9dad5d7fa0b0e4ee13
ashlesha8767 <166147937+ashlesha8767@users.noreply.github.com>
2024-04-22 16:19:47 +0530

    Add files via upload

---
+++ module-1/Assignment/script4.js
@@ -0,0 +1,21 @@
+console.log("Hello everyone")
+var num = prompt("Taking input as name");
+console.log(num);
+
+*{
+    margin: 0%;
+    padding: 0%;
+    font-family: sans-serif;
+box-sizing: border-box;
+}
+.js{
+    width: 100%;
+    min-height: 100vh;
+    background: linear-gradient(45deg,#121414,#620436);
+    color: #fff;
+    position: relative;
+    
+}
+.parent{
+    
+}
